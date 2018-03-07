ec1d99749a0e17178b78cebcae7f3115b1fe02bd
Shane Hower <shane.hower@gmail.com>
2018-03-07 15:32:47 -0500

    Update visualize_data.py

--- visualize_data.py
+++ visualize_data.py
@@ -5,23 +5,7 @@ import numpy as np
 df1 = pd.read_csv('/Users/shanehower/Desktop/vg_data/publisher_id.csv')
 df2 = pd.read_csv('/Users/shanehower/Desktop/vg_data/only_numbered_columns.csv')
 
-def conv_float(x,y):
-    x[y] = x[y].astype('float64')
-    return x[y]
-
-conv_float(df1,'Critic_Score')
-conv_float(df2,'Critic_Score')
-conv_float(df1,'Critic_Count')
-conv_float(df2, 'Critic_Count')
-conv_float(df1, 'User_Count')
-conv_float(df2, 'User_Count')
-conv_float(df1, 'User_Score')
-conv_float(df2, 'User_Score')
-
-feature_label = list(df2.columns.values)
-
 y = np.array(list(df1['Genre_Code']))
-X = np.array(list(df2))
 
 def plot_scatter(col1, col2):
     #first had to use SQL and add a column that was a number code for the genre.
