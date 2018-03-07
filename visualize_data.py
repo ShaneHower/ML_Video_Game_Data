c183bc3de7fa73507c565e3fbbde3d9240a42443
Shane Hower <shane.hower@gmail.com>
2018-03-07 16:01:17 -0500

    Update visualize_data.py

--- visualize_data.py
+++ visualize_data.py
@@ -7,19 +7,20 @@ df2 = pd.read_csv('/Users/shanehower/Desktop/vg_data/only_numbered_columns.csv')
 
 
 genre_label = list(df1['Genre'].unique())
-print(genre_label)
 
 y = np.array(list(df1['Genre_Code']))
 
 def plot_scatter(col1, col2):
     #first had to use SQL and add a column that was a number code for the genre.
     #then had to make a list of that column and convert it into a numpy array
-    #which will be a valid argument for color in matplotlib
+    #which for some reason is a valid argument for color in matplotlib
     formatter = plt.FuncFormatter(lambda i, *args: genre_label[int(i)])
     plt.scatter(df2[col1], df2[col2], c = y)
 
-    if col1 == 'year'  or col2 == 'year':
+    if col1 == 'year':
         plt.xlim(1975,2020)
+    elif col2 == 'year':
+        plt.ylim(1975,2020)
     plt.colorbar(ticks=range(12), format= formatter)
     plt.title("{0} vs. {1}".format(col1, col2))
     plt.xlabel(col1)
@@ -27,5 +28,5 @@ def plot_scatter(col1, col2):
     plt.tight_layout()
     plt.show()
 
-plot_scatter('year', 'publisher_ID')
+plot_scatter('publisher_ID', 'year')
 
