a2210848ba99d6bd11659534e78657748812832e
Shane Hower <shane.hower@gmail.com>
2018-03-07 15:57:12 -0500

    Update visualize_data.py

--- visualize_data.py
+++ visualize_data.py
@@ -14,7 +14,7 @@ y = np.array(list(df1['Genre_Code']))
 def plot_scatter(col1, col2):
     #first had to use SQL and add a column that was a number code for the genre.
     #then had to make a list of that column and convert it into a numpy array
-    #which for some reason is a valid argument for color in matplotlib
+    #which will be a valid argument for color in matplotlib
     formatter = plt.FuncFormatter(lambda i, *args: genre_label[int(i)])
     plt.scatter(df2[col1], df2[col2], c = y)
 
